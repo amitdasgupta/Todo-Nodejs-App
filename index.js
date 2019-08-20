5d60ebcf6c5ab1cf451673d7f20934db94703d22
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-08-20 18:49:22 +0530

    Modify homepage and add style to it

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port=8000;
 const app=express();
 // set up view engine
 app.set('view engine','ejs');
-app.set('view','./view')
+app.set('views','./view')
 
 app.use('/',require('./routes'));
-
+app.use(express.static('./assets'))
 
 app.listen(port,function(err){
    if(err){
