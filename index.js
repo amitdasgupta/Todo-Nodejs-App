17f1d0aef48fec690f0b44fca007f709e4c7af7e
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-08-20 15:46:01 +0530

    Add Todo Homepage

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const port=8000;
 const app=express();
+// set up view engine
+app.set('view engine','ejs');
+app.set('view','./view')
+
+app.use('/',require('./routes'));
+
 
 app.listen(port,function(err){
    if(err){
